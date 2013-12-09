8e0a45ec16affd0cd2c1eb13a83cbf489c831e0b
Pim Meijer <info@pimmeijer.com>
2013-12-09 16:19:49 +0100

    first graph with dummy data added

--- app/js/directives.js
+++ app/js/directives.js
@@ -6,9 +6,24 @@
 angular.module('footballTopscorers.directives', []).
   	directive('topscorerBubbleChart', function() {
 	    return {
-            restrict: 'E', // Directive Scope is Attribute
+            restrict: 'A', // Directive Scope is Attribute
             link: function (scope, elem, attrs) {
-            	console.log(scope);
+
+            	var arr = [
+	            	[11, 123, 1236, {label:"Acura", color:'sandybrown'}],
+				    [45, 92, 1067, {label:"Alfa Romeo", color:'skyblue'}],
+				    [24, 104, 1176, {label:"AM General", color:"salmon"}], [50, 23, 610, {color:"papayawhip"}],
+				    [18, 17, 539, "Audi"], [7, 89, 864], [2, 13, 1026, "Bugatti"]
+				];
+
+			    var topscorersBubblePlot;
+
+			    topscorersBubblePlot = $.jqplot(attrs.id,[arr],{
+			        title: 'Tospcorers goals',
+			        seriesDefaults:{
+			            renderer: $.jqplot.BubbleRenderer
+			        }
+			    });
     		},
 		};
 	});
