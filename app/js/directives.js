1dd51008ced3db90fc1e8816996d7ffa55d872b9
Pim Meijer <info@pimmeijer.com>
2013-12-09 16:07:34 +0100

    layout added

--- app/js/directives.js
+++ app/js/directives.js
@@ -5,7 +5,6 @@
 
 angular.module('footballTopscorers.directives', []).
   	directive('topscorerBubbleChart', function() {
-  		console.log('eded');
 	    return {
             restrict: 'E', // Directive Scope is Attribute
             link: function (scope, elem, attrs) {
